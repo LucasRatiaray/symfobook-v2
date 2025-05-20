7d71cf79ff19d8bd6c092b824c96e0a1a07d839b
Lucas Ratiaray <dev@lucasratiaray.fr>
2025-05-20 23:06:05 +0200

    feat: Enhance home page layout and statistics display; update book and login templates for improved UI

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\BookRepository;
+use App\Repository\CommentRepository;
 use App\Repository\UserRepository;
 use App\Repository\DiscussionRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(BookRepository $bookRepository, UserRepository $userRepository, DiscussionRepository $discussionRepository): Response
+    public function index(BookRepository $bookRepository, UserRepository $userRepository, DiscussionRepository $discussionRepository, CommentRepository $commentRepository): Response
     {
-        $latestBooks = $bookRepository->findBy([], ['id' => 'DESC'], 3);
+        $latestBooks = $bookRepository->findBy([], ['id' => 'DESC'], 4);
 
         $countBooks = $bookRepository->count([]);
         $countBooks = $countBooks > 0 ? $countBooks : 0;
@@ -25,11 +26,15 @@ final class HomeController extends AbstractController
         $countDiscussions = $discussionRepository->count([]);
         $countDiscussions = $countDiscussions > 0 ? $countDiscussions : 0;
 
+        $countComments = $commentRepository->count([]);
+        $countComments = $countComments > 0 ? $countComments : 0;
+
         return $this->render('home/index.html.twig', [
             'latestBooks' => $latestBooks,
             'countBooks' => $countBooks,
             'countUsers' => $countUsers,
             'countDiscussions' => $countDiscussions,
+            'countComments' => $countComments,
         ]);
     }
 }
